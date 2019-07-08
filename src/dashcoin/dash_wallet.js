798d72786cfa7425b2c8a13f641750631f3e0809
joy <joy.lai@zg.com>
2019-07-08 20:37:47 +0800

    add

--- src/dashcoin/dash_wallet.js
+++
@@ -1,8 +0,0 @@
-import BaseBitcoreLib from "../base/base_bitcore_lib";
-
-export default class DashWalletHelper extends BaseBitcoreLib {
-  constructor() {
-    super()
-    this._bitcoin = require('dashcore-lib')
-  }
-}
--- src/ulord/ulord_wallet1.js
+++
@@ -1,8 +0,0 @@
-import BaseBitcoreLib from "../base/base_bitcore_lib";
-
-export default class UlordWalletHelper1 extends BaseBitcoreLib {
-  constructor() {
-    super()
-    this._bitcoin = require('ulord-bitcore-lib')
-  }
-}
