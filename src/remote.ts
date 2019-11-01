b5d56608ad0495d023bcc9a7258afd639ec5c9b1
joy <joy.lai@zg.com>
2019-11-01 11:16:53 +0800

    add

--- src/remote.ts
+++ src/remote.ts
@@ -1,5 +1,6 @@
 import '@pefish/js-node-assist'
 import Client from 'bitcoin-core'
+import ErrorHelper from '@pefish/js-error';
 
 export interface RemoteConfig {
   host: string
@@ -19,5 +20,30 @@ export default class Remote {
 
   async request (method: string, params: any[]): Promise<any> {
     return this.client.command(method, ...params)
+  }
+
+  /**
+   * 评估交易手续费
+   * @param txSize raw transaction的字节数，长度除以2
+   * @param upNetworkFee 上限
+   * @param downNetworkFee 下限
+   */
+  public async estimateNetworkFee (txSize: string, upNetworkFee: string, downNetworkFee: string = `0.00004`): Promise<string> {
+    let networkFee: string
+    let feerate = (await this.client.estimateSmartFee(20)).feerate.toString();
+    if (feerate.lt_(0)) {
+      throw new ErrorHelper(`estimateSmartFee access error`)
+    }
+    const proposeNetworkFee = feerate.multi_(txSize).div_(1000).multi_(1.2)
+    if (!feerate) {
+      networkFee = downNetworkFee.add_(upNetworkFee).div_(2)
+    } else if (proposeNetworkFee.lt_(downNetworkFee)) {
+      networkFee = downNetworkFee
+    } else if (proposeNetworkFee.gt_(upNetworkFee)) {
+      networkFee = upNetworkFee;
+    } else {
+      networkFee = proposeNetworkFee
+    }
+    return networkFee
   }
 }
